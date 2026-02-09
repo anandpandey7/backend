f98e62251cd65c4de5170c0dc40bd317392ef055
Anand Raj <141954345+anandpandey7@users.noreply.github.com>
2026-02-09 06:07:11 +0530

    Update db.js

--- Api/src/config/db.js
+++ Api/src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://adminX:LugO7N9je8oK7SZr@cluster0.fzbgr1d.mongodb.net/dharti_automation");
+    await mongoose.connect("Your_URL");
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB error:", error.message);
